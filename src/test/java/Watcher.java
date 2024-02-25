adff7405b2c290c97d7171390e273fda87e5833f
HelenMunny <mahmudahelen.munny@gmail.com>
2024-02-25 04:09:02 -0500

    screenshot IO listener

--- src/test/java/Watcher.java
+++ src/test/java/Watcher.java
@@ -1,5 +1,14 @@
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Watcher implements ITestListener {
     @Override
@@ -9,7 +18,19 @@ public class Watcher implements ITestListener {
 
     @Override
     public void onTestFailure(ITestResult result) {
-        System.out.println("Failed");
+        System.out.println("Failed!");
+
+       DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd/HH-mm-ss");
+       Date date = new Date();
+       String name = dateFormat.format(date);
+       File file = ((TakesScreenshot) BaseClass.driver).getScreenshotAs(OutputType.FILE);
+       try{
+          FileUtils.copyFile(file, new File("src/screenshot/"+name+".png"));
+       }
+       catch(IOException e){
+           throw new RuntimeException(e);
+       }
+
     }
 
     @Override
